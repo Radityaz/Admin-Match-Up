6cc45875c65659b4a411cd2f0db28db393e78ae3
Agung_Pradana_08 <izunna158@gmail.com>
2023-08-15 07:32:40 +0700

    [Agung] Adding Relasi

---
+++ app/Models/Kompetisi.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Kompetisi extends Model
+{
+    use HasFactory;
+    protected $table = 'kompetisi';
+    protected $guarded = [];
+    
+
+    public function maps(){
+        return $this->hasMany(Map::class);
+    }
+}
