af7394c029bbcd0356b9ddec3d3c3ea74ce2c8c7
Agung_Pradana_08 <izunna158@gmail.com>
2023-08-21 20:23:41 +0700

    [Agung] Improve

--- app/Http/Controllers/StatistikController.php
+++ app/Http/Controllers/StatistikController.php
@@ -3,14 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class StatistikController extends Controller
 {
     public function index(){
+
+        $pengguna = Auth::user();
         $user = DB::table('users')->get();
+        $sparring = DB::table('usersparring')->get();
+        $mabar = DB::table('usermabar')->get();
+        $tim = DB::table('usertim')->get();
+        
         session()->flash('page', 'statistik');
         // dd($user);
-        return view('home.statistik.home', compact(['user']));
+        return view('home.statistik.home', compact(['user', 'sparring', 'mabar', 'tim', 'pengguna']));
     }
+
 }
