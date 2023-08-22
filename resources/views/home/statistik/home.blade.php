459fe9a5d0fa9cdf762c48ab5f6826d63ba8a6d1
Your Name <you@example.com>
2023-08-22 07:01:08 +0700

    [Riko] Adding Things

--- resources/views/home/statistik/home.blade.php
+++ resources/views/home/statistik/home.blade.php
@@ -2,18 +2,15 @@
 
 @section('content')
 
-<div class="info-box">
-  <h2>Halo, {{Auth::user()->name}}</h2>
-  <p>Selamat datang di page admin</p>
-</div>
-
-  <div class="container w-100 h-100 p-3">
-    <h3></h3>
-    <p></p>
-    <div id="statistik-wrapper" class="p-1" >
-      <div class="box p-2 d-flex shadow-regular flex-column justify-content-between" style="border-radius: 8px" >
+  <div class="container p-3 " style="height: 100vh" >
+    <div class="info-box">
+      <h2>Halo, {{Auth::user()->name}}</h2>
+      <p>Selamat datang di page admin</p>
+    </div>
+    <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>
+    <div id="statistik-wrapper" class="p-3"  >
+      <div class="box p-2 d-flex shadow-regular flex-column justify-content-between text-light " style="border-radius: 8px ; background: #c72418;" >
         <div class="d-flex align-items-center" >
-          <img class="blank logo-sm rounded-circle" src="" alt="">
           <h6 class="d-inline-block p-0 m-0 ms-2 fw-bold" >Jumlah Pengguna</h6>
         </div>
         <div>
@@ -22,20 +19,18 @@
         </div>
       </div>
 
-      <div class="box p-2 d-flex flex-column shadow-regular justify-content-between" style="border-radius: 8px" >
+      <div class="box p-2 d-flex flex-column shadow-regular justify-content-between text-light " style="border-radius: 8px ; background: #67c916" >
         <div class="d-flex align-items-center" >
-          <img class="blank logo-sm rounded-circle" src="" alt="">
           <h6 class="d-inline-block p-0 m-0 ms-2 fw-bold" >Jumlah Sparring</h6>
         </div>
         <div>
           <h1 class="d-inline-block p-0 m-0" >{{$sparring->count()}}</h1>
-          <p class="d-inline-block p-0 m-0" >akun</p>
+          <p class="d-inline-block p-0 m-0" > Event</p>
         </div>
       </div>
 
-      <div class="box p-2 d-flex flex-column shadow-regular justify-content-between" style="border-radius: 8px" >
+      <div class="box p-2 d-flex flex-column shadow-regular justify-content-between text-light " style="border-radius: 8px; background: #1641cc;" >
         <div class="d-flex align-items-center" >
-          <img class="blank logo-sm rounded-circle" src="" alt="">
           <h6 class="d-inline-block p-0 m-0 ms-2 fw-bold" >Jumlah Mabar</h6>
         </div>
         <div>
@@ -44,9 +39,8 @@
         </div>
       </div>
 
-      <div class="box p-2 d-flex flex-column shadow-regular justify-content-between" style="border-radius: 8px" >
+      <div class="box p-2 d-flex flex-column shadow-regular justify-content-between text-light " style="border-radius: 8px; background: #c4107c" >
         <div class="d-flex align-items-center" >
-          <img class="blank logo-sm rounded-circle" src="" alt="">
           <h6 class="d-inline-block p-0 m-0 ms-2 fw-bold" >Jumlah Tim</h6>
         </div>
         <div>
@@ -55,7 +49,7 @@
         </div>
       </div>
     </div>
-    <div class="row p-3" style="height: 45%;" >
+    {{-- <div class="row p-3" style="height: 45%;" >
       <div class="col-4 p-2 shadow-regular " style="border-radius: 8px" >
         olahraga Favorite
 
@@ -96,8 +90,76 @@
           </tbody>
         </table>
       </div>
-    </div>
+    </div> --}}
   </div>
   
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="../../assets/js/vendor/popper.min.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+
+    <!-- Icons -->
+    <script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
+    <script>
+      feather.replace()
+    </script>
+
+    <!-- Graphs -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js"></script>
+    <script>
+      var ctx = document.getElementById("myChart");
+      var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: ["21-08-2023", "22-08-2023",  ],
+          datasets: [
+            {
+              data: [ 0 , {{ $user->count() }} ],
+              lineTension: 0,
+              backgroundColor: 'transparent',
+              borderColor: '#c72418',
+              borderWidth: 4,
+              pointBackgroundColor: '#c72418'
+            },
+            {
+              data: [0, {{ $sparring->count() }}],
+              lineTension: 0,
+              backgroundColor: 'transparent',
+              borderColor: '#67c916', // Warna orange
+              borderWidth: 4,
+              pointBackgroundColor: '#67c916'
+            },
+            {
+              data: [0 , {{ $mabar->count() }}],
+              lineTension: 0,
+              backgroundColor: 'transparent',
+              borderColor: '#1641cc', // Warna orange
+              borderWidth: 4,
+              pointBackgroundColor: '#1641cc'
+            },
+            {
+              data: [0, {{ $tim->count() }}],
+              lineTension: 0,
+              backgroundColor: 'transparent',
+              borderColor: '#c4107c', // Warna orange
+              borderWidth: 4,
+              pointBackgroundColor: '#c4107c'
+            }
+          ]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: false
+              }
+            }]
+          },
+          legend: {
+            display: false,
+          }
+        }
+      });
+    </script>
 
 @endsection
